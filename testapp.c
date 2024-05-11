17b7aa1619c28c1e6ba5c1cea18ae44de4d35b24
Jahid Hassan <jhasan1743@gmail.com>
2024-05-11 17:11:54 +0600

    A generic basic Graph Ready

---
+++ testapp.c
@@ -0,0 +1,13 @@
+#include "graph.h"
+
+extern graph_t *build_first_topo();
+
+int main(int argc, char const *argv[])
+{
+    graph_t *topo = build_first_topo();
+
+
+    dump_graph(topo);
+
+    return 0;
+}
